29c1eff0b8ebcada255272b861d072108b5e95d0
nextupcredit-web <nextupcredit@gmail.com>
2026-09-20 18:30:07 -0400

    Update login.js

--- nextup-credit-pro/app/login.js
+++ nextup-credit-pro/app/login.js
@@ -243,12 +243,12 @@
       });
 
       var h1 = el('h1', { text: (c.first_name || '') + ' ' + (c.last_name || '') });
-      var repBox = el('div', { id: 'repbox' }); var letBox = el('div', { id: 'letbox' });
+            var repBox = el('div', { id: 'repbox' }); var letBox = el('div', { id: 'letbox' }); var othBox = el('div', {});
       show([el('div', { class: 'bar' }, [backBtn(), signOutBtn(true)]), h1, f,
         el('h2', { text: 'Private details' }),
-        el('p', { class: 'sub', text: 'Stored encrypted. Only revealed on request, and every reveal is recorded in the activity log.' }), out, rev, sf, repBox, letBox]);
+                el('p', { class: 'sub', text: 'Stored encrypted. Only revealed on request, and every reveal is recorded in the activity log.' }), out, rev, sf, repBox, letBox, othBox]);
       if (window.NCPReport) window.NCPReport.mount(repBox, { sb: sb, clientId: id, prof: prof, user: user });
-      if (window.NCPLetters) window.NCPLetters.mount(letBox, { sb: sb, clientId: id, prof: prof, user: user });
+            if (window.NCPOutbound) window.NCPOutbound.mount(othBox, { sb: sb, clientId: id, prof: prof, user: user });
     }
   }
 
